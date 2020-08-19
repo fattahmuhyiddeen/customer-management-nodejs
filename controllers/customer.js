6041042b3242f52fd8a13ddc59c85c28fee8f6da
Fattah Muhyiddeen <fattahmuhyiddeen@gmail.com>
2020-08-20 00:38:05 +0800

    bug fix

--- controllers/customer.js
+++ controllers/customer.js
@@ -14,9 +14,21 @@ function create(req, res) {
   }).catch(function (err) {
     console.log("create failed with error: " + err);
     res.sendStatus(500);
-    // res.send(err);
     return 0;
   });
 }
 
-module.exports = { create };
+function getAll(req, res) {
+  db.customer.findAll().then(data => {
+    console.log('data', data)
+    // Send created user to client
+    // res.send(data.id);
+    res.send({ data });
+  }).catch(function (err) {
+    console.log("create failed with error: " + err);
+    res.sendStatus(500);
+    return 0;
+  });
+}
+
+module.exports = { create, getAll };
--- routes/customer.js
+++ routes/customer.js
@@ -3,10 +3,5 @@ var router = express.Router();
 const customer = require('../controllers/customer')
 // Home page route.
 router.post('/', customer.create);
-
-// About page route.
-router.get('/about', function (req, res) {
-  res.send('About this system');
-})
-
+router.get('/', customer.getAll);
 module.exports = router;
