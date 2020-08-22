ca9312c22eb5938d798b1fb03aca859bbef17598
Fattah Muhyiddeen <fattahmuhyiddeen@gmail.com>
2020-08-23 07:31:56 +0800

    bug fix

---
+++ database/models/task.js
@@ -0,0 +1,33 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Task extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  Task.init({
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    customerId: {
+      type: DataTypes.INTEGER
+    },
+    date: DataTypes.DATE,
+    createdAt: { type: DataTypes.DATE, defaultValue: DataTypes.NOW },
+    updatedAt: { type: DataTypes.DATE, defaultValue: DataTypes.NOW },
+  }, {
+    sequelize,
+    modelName: 'Task',
+  });
+  return Task;
+};
